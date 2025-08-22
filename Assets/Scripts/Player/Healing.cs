28f914820c33f662b641292b97f64f406988d732
Ade1ard <gorbunovjarroslav@gmail.com>
2025-08-22 13:28:33 +0300

    HealFlackUI

--- Assets/Scripts/Player/Healing.cs
+++ Assets/Scripts/Player/Healing.cs
@@ -1,16 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class Healing : MonoBehaviour
 {
-    private Animator _animator;
-    private PlayerHealth _health;
+    [SerializeField] private float _FlackCount;
 
     [SerializeField] private float _healHPCount;
     [SerializeField] private GameObject _healFlask;
+    [SerializeField] private TextMeshProUGUI _FlackCountText;
+
+    private Animator _animator;
+    private PlayerHealth _health;
+
     void Start()
     {
         _animator = GetComponent<Animator>();
         _health = GetComponent<PlayerHealth>();
+        _FlackCountText.text = _FlackCount.ToString();
     }
 
     void Update()
@@ -23,18 +29,16 @@ public class Healing : MonoBehaviour
 
     private void Heal() //called by events in animations
     {
-        _health.AddHealt(_healHPCount);
+        if (_FlackCount > 0)
+        {
+            _health.AddHealt(_healHPCount);
+            _FlackCount -= 1;
+            _FlackCountText.text = _FlackCount.ToString();
+        }
     }
 
     private void IsHealing(int _int) //called by events in animations
     {
-        if (_int == 1)
-        {
-            _healFlask.SetActive(true);
-        }
-        else
-        {
-            _healFlask.SetActive(false);
-        }
+        _healFlask.SetActive(_int == 1 ? true : false);
     }
 }
