d54259511a67f7c6832cb57aaacf314ffe944cbe
Ade1ard <gorbunovjarroslav@gmail.com>
2025-08-22 14:46:55 +0300

    effectsAsset

--- Assets/Scripts/Player/Healing.cs
+++ Assets/Scripts/Player/Healing.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class Healing : MonoBehaviour
 {
     [SerializeField] private float _FlackCount;
-
     [SerializeField] private float _healHPCount;
+
+    [SerializeField] private ParticleSystem _effect;
+
     [SerializeField] private GameObject _healFlask;
     [SerializeField] private TextMeshProUGUI _FlackCountText;
 
@@ -34,6 +36,7 @@ public class Healing : MonoBehaviour
             _health.AddHealt(_healHPCount);
             _FlackCount -= 1;
             _FlackCountText.text = _FlackCount.ToString();
+            Instantiate(_effect, transform.position + Vector3.up * 1.5f, Quaternion.identity);
         }
     }
 
