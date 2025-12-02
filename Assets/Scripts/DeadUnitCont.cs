e5f0cf5272b95b38864a6f92e5d9968975ff3325
Ade1ard <gorbunovjarroslav@gmail.com>
2025-12-02 22:04:02 +0300

    DeadUnits_2/3

--- Assets/Scripts/DeadUnitCont.cs
+++ Assets/Scripts/DeadUnitCont.cs
@@ -6,6 +6,14 @@ public class DeadUnitCont : MonoBehaviour
     {
         Vector3 dir = new Vector3(Random.Range(0f, 360f), 0, Random.Range(0, 360));
         transform.rotation = Quaternion.LookRotation(dir);
+
         GetComponent<Animator>().SetFloat("Dead", Random.Range(0.1f, 4f));
+
+        float Scale = Random.Range(0.9f, 1.7f);
+        transform.localScale = new Vector3(Scale, Scale, Scale);
+
+        gameObject.isStatic = true;
+        foreach (SkinnedMeshRenderer SMR in GetComponentsInChildren<SkinnedMeshRenderer>())
+            SMR.gameObject.isStatic = true;
     }
 }
