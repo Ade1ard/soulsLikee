a1b842a04aa5e2d12bfbbc7f4fee4313f8c997d6
Ade1ard <gorbunovjarroslav@gmail.com>
2025-12-03 20:20:39 +0300

    DeadUnits on arena and hall complete

--- Assets/Scripts/DeadUnitCont.cs
+++ Assets/Scripts/DeadUnitCont.cs
@@ -1,19 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DeadUnitCont : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> DeadUnitsMeshes = new List<GameObject>();
+
     void Start()
     {
+        DeadUnitsMeshes[Random.Range(0, DeadUnitsMeshes.Count)].SetActive(true);
+
         Vector3 dir = new Vector3(Random.Range(0f, 360f), 0, Random.Range(0, 360));
         transform.rotation = Quaternion.LookRotation(dir);
 
-        GetComponent<Animator>().SetFloat("Dead", Random.Range(0.1f, 4f));
-
         float Scale = Random.Range(0.9f, 1.7f);
         transform.localScale = new Vector3(Scale, Scale, Scale);
-
-        gameObject.isStatic = true;
-        foreach (SkinnedMeshRenderer SMR in GetComponentsInChildren<SkinnedMeshRenderer>())
-            SMR.gameObject.isStatic = true;
     }
 }
